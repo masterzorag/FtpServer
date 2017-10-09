248fed1a0aa6c10d1dc776ccea7dad719fc457b4
masterzorag <masterzorag@gmail.com>
2017-10-09 23:26:12 +0200

    simplify main

--- source/main.c
+++ source/main.c
@@ -2,30 +2,22 @@
 #include <string.h>
 #include <stdlib.h>
 #include "ftpserver.h"
+
 int main(int argc, char**argv)
 {
     struct FtpServer *s = (struct FtpServer*) malloc(sizeof(struct FtpServer));
-    if(argc >= 2)
-    {
-    	show_log(argv[1]);
-    s->_port = atoi(argv[2]);
-    }
-    else
-    {
-    	 s->_port = 21;
-    }
-    if(argc < 3)
-    //strcpy(s->_relative_path, argv[2]);
-    {
-    	strcpy(s->_relative_path, "/tmp");
-    }
-    else
-    {
-    	show_log(argv[2]);
-    	 strcpy(s->_relative_path, argv[4]);
-    }
+    if(!s) return -1;
+
+    s->_port = 21;
+    strcpy(s->_relative_path, "/");
+    
+    //show_log("log");
+    
     init_ftp_server(s);
+
     start_ftp_server(s);
-   // close_ftp_server(s);
+
+    //close_ftp_server(s);
+
     return 0;
 }
